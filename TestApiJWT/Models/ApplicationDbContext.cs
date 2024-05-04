36f26a6d9a3ed9589ecd3e9802594f3280e512d2
EL CONSUL <EL CONSUL@HUDA>
2024-05-04 21:48:22 +0300

    add

---
+++ TestApiJWT/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace TestApiJWT.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ TestApiJWT/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApiJWT.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+        public List<RefreshToken> RefreshTokens { get; set; }
+    }
+}
